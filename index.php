47c18009ca92eb83c9633c1cedf70379d63aa990
manimalladi <manimalladi05@gmail.com>
2025-08-14 17:26:54 +0530

    changes

--- index.php
+++ index.php
@@ -412,6 +412,11 @@
 							<img class="image-sild" src="images/festivel_sliders/subhash_slider_independence.jpg" alt="newyear ">
 						</div>
 					</div>
+					<div class="slide">
+						<div class="img--holder" alt="subhas"  style="  max-width: 100%; height:auto;">
+							<img class="image-sild" src="images/festivel_sliders/sree_krishna_janmashtami.jpg" alt="newyear ">
+						</div>
+					</div>
 					 
 					<div class="slide">
 						<div class="img--holder" data-bg="images/sliders/micro_needling.png" alt="subhas" style="  max-width: 100%;">
