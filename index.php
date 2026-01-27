c997ca9392bb1e898bff2a75e567885a97940c2d
manimalladi <manimalladi05@gmail.com>
2026-01-27 11:07:21 -0800

    to remove the slider for website

--- index.php
+++ index.php
@@ -404,12 +404,12 @@
 					data-slick='{"arrows": false, "dots": true}'>
 
 
-				<div class="slide">
+				<!--<div class="slide">
 						 <div class="img--holder" alt="subhas"  style="  max-width: 100%; height:auto;">
 							<img class="image-sild" src="images/festivel_sliders/republic_day_2026.jpeg" alt="newyear ">
 						</div> 
 					</div>
-				<!--		<div class="slide">
+						<div class="slide">
 						<div class="img--holder" alt="subhas"  style="  max-width: 100%; height:auto;">
 							<img class="image-sild" src="images/festivel_sliders/sree_krishna_janmashtami.jpg" alt="newyear ">
 						</div>
